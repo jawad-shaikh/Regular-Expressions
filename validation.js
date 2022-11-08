7cbf380a84d9c43cc45eaa42506808da7fd129a8
jawad-shaikh <shaikhjawad314@gmail.com>
2022-11-09 01:40:47 +0500

    added comments

--- validation.js
+++ validation.js
@@ -1,13 +1,16 @@
 const inputs = document.querySelectorAll("input");
 
+// regex patterns
 const regex = {
   username: /^[a-zA-Z\d_]{5,12}$/,
   email: /^([a-z\d\.-]+)@([a-z\d-]{5,20})\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
+  //        yourname    @     domain      .  com          ( .uk )
   password: /^[a-zA-Z\d_\-@]{8,20}$/,
   telephone: /^[\d]{11}$/,
   slug: /^[a-z\d\-]{8,20}$/,
 };
 
+// validation function
 const validate = (elem, regex) => {
   console.log(elem);
   if (regex.test(elem.value)) {
@@ -19,6 +22,7 @@ const validate = (elem, regex) => {
   }
 };
 
+// attach keyup events to inputs
 inputs.forEach((input) => {
   input.addEventListener("keyup", (e) => {
     validate(e.target, regex[e.target.getAttribute("name")]);
